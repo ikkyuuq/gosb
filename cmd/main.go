45dfa56da9d8c61bd5b22705cc2c026d517655af
ikkyuuq <kittipongprasompong@gmail.com>
2024-07-26 06:41:14 +0700

    feat(main): tea.NewProgram().Run()

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"go-structure-builder/pkg/builder"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
 func main() {
+	program := tea.NewProgram(builder.NewBuilder())
+	if _, err := program.Run(); err != nil {
+		fmt.Printf("Oh no %v", err)
+		os.Exit(1)
+	}
 }
